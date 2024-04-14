c620abbcaeac088fdc784d614655af8d5f0d668d
rysz <konrad.michal.rysz@gmail.com>
2024-04-14 15:57:25 +0200

    GET, POST, DELETE, UPDATE - aktualizacja danych

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -21,5 +22,25 @@ public class ParticipantService {
 		Query query = connector.getSession().createQuery(hql);
 		return query.list();
 	}
+	public Participant findByLogin(String login) {
+		return connector.getSession().get(Participant.class, login);
+	}
 
+	public void add(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+	}
+
+	public void remove(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().remove(participant);
+		transaction.commit();
+	}
+
+	public void update(Participant updatedParticipant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().merge(updatedParticipant);
+		transaction.commit();
+	}
 }
